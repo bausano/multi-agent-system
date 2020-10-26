4f680852783f95854eab9e1b1a25fdfa913f4630
Michael <bausanomichal@gmail.com>
2020-10-26 00:20:57 +0000

    Testing WS connection.

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+`source ./venv/bin/activate`
+`black src`
+
+`pip install websockets asyncio keras-rl`
+
+TODO: Docker compose which runs mc server, mc adapter and the bots.
+
+
+https://github.com/dennybritz/reinforcement-learning
+https://github.com/keras-rl/keras-rl/tree/master/examples
+https://stackoverflow.com/questions/44469266/how-to-implement-custom-environment-in-keras-rl-openai-gym
+https://github.com/openai/gym/blob/master/gym/envs/toy_text/hotter_colder.py
---
+++ src/main.py
@@ -0,0 +1,41 @@
+import json
+import asyncio
+import websockets
+
+
+async def connect_to_adapter():
+    uri = "ws://localhost:8999"
+    async with websockets.connect(uri) as websocket:
+        assert is_ok(await recv_message(websocket)), "Cannot connect to the adapter"
+        print("Connected to MC adapter WS server.")
+        await websocket.send(init_message("joe", "localhost", 35591))
+        assert is_ok(await recv_message(websocket)), "Cannot connect to MC server"
+        print("ZZZzzzz...")
+        await asyncio.sleep(10)
+
+
+async def recv_message(websocket):
+    return json.loads(await websocket.recv())
+
+
+def is_ok(message):
+    if not "ok" in message:
+        print("Expected ok message, got: ")
+        print(message)
+    return "ok" in message
+
+
+def init_message(username, host, port):
+    return json.dumps(
+        {
+            "init": {
+                "username": username,
+                "host": host,
+                "port": port,
+                "behavior": "run-and-hit",
+            }
+        }
+    )
+
+
+asyncio.get_event_loop().run_until_complete(connect_to_adapter())
